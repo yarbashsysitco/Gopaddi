e01ba434c974ba1cf4e43fc23f98c37562cfcf4a
Aparna <aparna.sysitco@gmail.com>
2024-02-28 19:14:28 +0530

    profileinfoVC updated

--- Gopaddi/VC/Profile/VC/ProfileInfoVC.swift
+++ Gopaddi/VC/Profile/VC/ProfileInfoVC.swift
@@ -9,23 +9,24 @@ import UIKit
 
 class ProfileInfoVC: UIViewController {
 
+    @IBOutlet weak var BirthView: UIView!
+    @IBOutlet weak var LocationView: UIView!
+    @IBOutlet weak var BioView: UIView!
+    @IBOutlet weak var NameView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("ok")
+        borders(view: BirthView, color: .lightGray, width: 2, radius: 5)
+        borders(view: LocationView, color: .lightGray, width: 2, radius: 5)
+        borders(view: BioView, color: .lightGray, width: 2, radius: 5)
+        borders(view: NameView, color: .lightGray, width: 2, radius: 5)
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func borders(view: UIView,color: UIColor, width: CGFloat, radius: CGFloat){
+        view.layer.borderColor = color.cgColor
+        view.layer.borderWidth = width
+        view.layer.cornerRadius = radius
     }
-    */
 
 }
