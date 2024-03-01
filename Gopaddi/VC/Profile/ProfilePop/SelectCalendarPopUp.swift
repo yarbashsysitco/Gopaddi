5eae9b8a415035d1539a798c58c54b884dbf7694
Aparna <aparna.sysitco@gmail.com>
2024-03-01 14:49:36 +0530

    update

--- Gopaddi/VC/Profile/ProfilePop/SelectCalendarPopUp.swift
+++ Gopaddi/VC/Profile/ProfilePop/SelectCalendarPopUp.swift
@@ -10,10 +10,11 @@ import UIKit
 class SelectCalendarPopUp: UIViewController {
     @IBOutlet weak var cencelBtn: UIButton!
     
+    @IBOutlet weak var picker: UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
         cencelBtn.layer.cornerRadius = 5
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func cencelBtnss(_ sender: Any) {
