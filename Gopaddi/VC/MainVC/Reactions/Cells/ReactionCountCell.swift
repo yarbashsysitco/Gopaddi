1775bc2163cdd4fe4b840ece814f3973a38ee1e4
yarbash <yarabash.systico@gmail.com>
2024-03-04 17:33:13 +0530

    Rections vc

---
+++ Gopaddi/VC/MainVC/Reactions/Cells/ReactionCountCell.swift
@@ -0,0 +1,17 @@
+//
+//  ReactionCountCell.swift
+//  Gopaddi
+//
+//  Created by admin on 04/03/24.
+//
+
+import UIKit
+
+class ReactionCountCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
