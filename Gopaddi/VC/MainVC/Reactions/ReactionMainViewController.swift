a7c89de2c44f814cfaf0c365b27ab89150c45e2b
Aparna <aparna.sysitco@gmail.com>
2024-03-05 11:46:21 +0530

    recactionvc ui updated

--- Gopaddi/VC/MainVC/Reactions/ReactionMainViewController.swift
+++ Gopaddi/VC/MainVC/Reactions/ReactionMainViewController.swift
@@ -7,19 +7,28 @@
  
 import UIKit
  
-class ReactionMainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
+class ReactionMainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UITableViewDelegate, UITableViewDataSource {
+   
     var isselect = Int()
     var emojies = ["all","emo1","emo2","emo3","emo4","emo5","emo6"]
+    @IBOutlet weak var tableview: UITableView!
     @IBOutlet weak var collection: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
  
         collection.delegate = self
         collection.dataSource = self
-//        self.collection.register(UINib.init(nibName: "ReactionCountCollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "ReactionCountCollectionViewCell")
         collection.register(UINib.init(nibName: "ReactionCountCell", bundle: .main), forCellWithReuseIdentifier: "ReactionCountCell")
+        
+        tableview.delegate =  self
+        tableview.dataSource = self
+        tableview.register(UINib.init(nibName: "ViewReactionCell", bundle: .main), forCellReuseIdentifier: "ViewReactionCell")
     }
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+        self.dismiss(animated: true)
+    }
+    
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return emojies.count
@@ -28,14 +37,14 @@ class ReactionMainViewController: UIViewController, UICollectionViewDelegate, UI
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collection.dequeueReusableCell(withReuseIdentifier: "ReactionCountCell", for: indexPath)as! ReactionCountCell
         cell.emojiImage.image = UIImage(named: emojies[indexPath.row])
-//        if isselect == indexPath.row{
-//            cell.emojiview.layer.borderWidth = 1
-//            cell.emojiview.layer.borderColor = UIColor.systemBlue.cgColor
-//        }else{
-//            cell.emojiview.layer.borderColor = UIColor.lightGray.cgColor
-//        }
-//
         cell.emojiCountLabel.text = "35"
+        
+        if isselect == indexPath.row{
+            cell.emojiview.layer.borderWidth = 1
+            cell.emojiview.layer.borderColor = UIColor.systemBlue.cgColor
+        }else{
+            cell.emojiview.layer.borderColor = UIColor.lightGray.cgColor
+        }
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -43,14 +52,20 @@ class ReactionMainViewController: UIViewController, UICollectionViewDelegate, UI
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         isselect = indexPath.row
-        let cell = collection.dequeueReusableCell(withReuseIdentifier: "ReactionCountCell", for: indexPath)as! ReactionCountCell
-        cell.emojiImage.image = UIImage(named: emojies[indexPath.row])
-        if isselect == indexPath.row{
-            cell.emojiview.layer.borderWidth = 1
-            cell.emojiview.layer.borderColor = UIColor.systemBlue.cgColor
-        }else{
-            cell.emojiview.layer.borderColor = UIColor.lightGray.cgColor
-        }
-        collection.reloadData()
+        collectionView.reloadData()
+    }
+    
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableview.dequeueReusableCell(withIdentifier: "ViewReactionCell", for: indexPath)as! ViewReactionCell
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
 }
---
+++ Gopaddi/VC/MainVC/Reactions/ViewReaction/ViewReactionCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViewReactionCell.swift
+//  Gopaddi
+//
+//  Created by jizan k on 05/03/24.
+//
+
+import UIKit
+
+class ViewReactionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
