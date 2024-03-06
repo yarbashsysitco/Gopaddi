12e40670a73b662f0b55368bb1c864bc12f46ad3
yarbash <yarabash.systico@gmail.com>
2024-03-06 14:39:24 +0530

    into ui

---
+++ Gopaddi/VC/intro/IntroViewController.swift
@@ -0,0 +1,29 @@
+//
+//  IntroViewController.swift
+//  Gopaddi
+//
+//  Created by admin on 06/03/24.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
